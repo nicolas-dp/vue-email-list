cc349838cd2fb3b0f50d57189f1b41ad67dac8b6
Nicolas <wellcomenicolas@gmail.com>
2022-04-29 16:35:59 +0200

    Add methods click

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const app = new Vue({
 
     data: {
         mail_random: [],
-        
+
     },
 
     methods: {
 
+        clickToGenerate(){
+            console.log(this.mail_random);
+        }
     },
 
     mounted() {
-        
+
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(response => {
 
                     
-                    this.mail_random.push(response.data.response)
-                    console.log(mail_random);
-
-
-
+                        this.mail_random.push(response.data.response)
+                        //console.log(this.mail_random);
 
                 })
         }
